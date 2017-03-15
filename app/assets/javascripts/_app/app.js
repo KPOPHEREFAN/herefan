88d64171d7be62841d59075a8b35343eb3c26c96
seohasong <tisutoo@gmail.com>
2017-03-15 12:30:54 +0900

    add dropdown_proto

--- app/assets/javascripts/_app/app.js
+++ app/assets/javascripts/_app/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	absolute_header_height = parseInt($("#absolute_header").css("height"));
 	var fix_header_height = parseInt($("#header").css("height"));
+	$("#main").css("padding-top", absolute_header_height+fix_header_height)
 	$("#header").css("top", absolute_header_height)
 	$("#absolute_header").css("height", absolute_header_height+fix_header_height/2)
 })
